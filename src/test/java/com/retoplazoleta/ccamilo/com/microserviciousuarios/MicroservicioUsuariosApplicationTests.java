55b126a9d91b967c898fc80b98a66f3d71c4f155
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2025-05-29 00:01:49 -0500

    Add se crea proyecto para HU

---
+++ src/test/java/com/retoplazoleta/ccamilo/com/microserviciousuarios/MicroservicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retoplazoleta.ccamilo.com.microserviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
